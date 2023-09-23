fe6c3d84517372dc2825dd91e2f6afdbd26a0818
profrichharris <rich.harris@bristol.ac.uk>
2023-09-23 11:47:30 +0100

    2023-09-23

--- MandM/scripts/script1.R
+++ MandM/scripts/script1.R
@@ -1,73 +1,71 @@
-# Load the required packages and install them if they are not available
-# The remotes package allows the development package ipsumexamples to be installed from github
+# Install any missing packages and require them
 
-while(!require(remotes)) install.packages("remotes", dependencies = TRUE)
-while(!require(ipumsr)) install.packages("ipumsr", dependencies = TRUE)
-while(!require(ipumsexamples)) install_github("ipums/ipumsr/ipumsexamples")
-while(!require(XML)) install.packages("XML", dependencies = TRUE)
-while(!require(tidyverse)) install.packages("tidyverse", dependencies = TRUE)
-while(!require(sf)) install.packages("sf", dependencies = TRUE)
-while(!require(ggplot2)) install.packages("ggplot2", dependencies = TRUE)
-while(!require(classInt)) install.packages("classInt", dependencies = TRUE)
-while(!require(ggspatial)) install.packages("ggspatial", dependencies = TRUE)
+installed <- installed.packages()[,1]
+pkgs <- c("XML", "tidyverse", "sf", "ggplot2",
+          "ggspatial", "cowplot")
+install <- pkgs[!(pkgs %in% installed)]
+if(length(install)) install.packages(install, dependencies = TRUE)
+invisible(lapply(pkgs, require, character.only = TRUE))
 
-# The rest of this code is adapted from the first parts of
-# https://cran.r-project.org/web/packages/ipumsr/vignettes/ipums-geography.html
-# I have simplified the code a little
+# Read-in the attribute data and the boundary file:
+df <- read_csv("https://github.com/profrichharris/profrichharris.github.io/blob/main/MandM/data/diversity.csv?raw=true")
+map <- st_read("https://github.com/profrichharris/profrichharris.github.io/raw/main/MandM/boundary%20files/cities.geojson", quiet = TRUE)
 
-fuel_missing_vals <- c(0, 60, 66, 70, 99)
-fuel_solid_vals <- c(50:56, 61, 73, 74, 75)
+# Although more complex, at heart what the following code does is
+# join the map to the data and then produce a separate map for
+# each city and time period, using a consistent style
 
-# Read in some example IPUMS data for individuals and then aggregate (group) it to the
-# geographic level, calculating the percentages using solid fuel for cooking in each year
-
-system.file("extdata", "ipumsi_00011.xml", package = "ipumsexamples") |>
-  read_ipums_micro(verbose = FALSE) |>
-  filter(COUNTRY == 170) |>
-  select(COUNTRY, GEOLEV1, YEAR, PERWT, FUELCOOK) |>
-  mutate(SOLIDFUEL = ifelse(FUELCOOK %in% fuel_solid_vals, 1, 0)) |>
-  filter(!(FUELCOOK %in% fuel_missing_vals)) |>
-  group_by(COUNTRY, GEOLEV1, YEAR) |>
-  summarise(SOLIDFUELWT = weighted.mean(SOLIDFUEL, w = PERWT), SOLIDFUEL = mean(SOLIDFUEL), .groups = "keep") |>
-  mutate(across(starts_with("SOLID"), ~ . * 100)) ->
-  ipumsi_summary
- 
-# Read in the corresponding boundary file of the geographic areas
-# Also simplify (remove vertices from) the outlines of the areas, which will make
-# plotting them faster
-
-system.file("extdata", "geo1_co1964_2005.zip", package = "ipumsexamples") %>%
-  read_ipums_sf(., verbose = FALSE) %>%
-  st_simplify(dTolerance = 100, preserveTopology = TRUE) -> 
-  colombia_shape
-
-# Join the boundary file to the attribute data
-# It uses one of the in-built helper functions in the ipumsr package to help
-# make the handling of the IPSUM data & boundary files easier
+df |>
+  pivot_longer(where(is.numeric), values_to = "index", names_to = "year") %>%
+  mutate(year = paste0("20",substring(year, 3, 4))) %>%
+  left_join(map, ., by = "OAXXCD") %>%
+  mutate(group = paste(CITY, year, sep = " ~ ")) %>%
+  split(.$group) %>%
   
-ipumsi <- ipums_shape_inner_join(
-    ipumsi_summary, 
-    colombia_shape,
-    by = c("GEOLEV1" = "GEOLEVEL1")
-)
+  lapply(function(x) {
+    
+    ggplot(x, aes(fill = index)) +
+      geom_sf(col = "transparent") +
+      scale_fill_viridis_c("Diversity",
+                           values = c(0,0.25,0.5,0.7,0.85,0.95,1)) +
+      annotation_north_arrow(location = "tl",
+                             style = north_arrow_minimal(text_size = 10),
+                             height = unit(0.6, "cm"), width = unit(0.6, "cm")) +
+      annotation_scale(location = "br", style = "ticks", line_width = 0.5,
+                       text_cex = 0.5, tick_height = 0.4,
+                       height = unit(0.15, "cm"), text_pad = unit(0.10, "cm")) +
+      theme_minimal() +
+      theme(axis.text = element_blank(),
+            axis.ticks = element_blank(),
+            plot.title = element_text(size = 8, hjust = 0.5),
+            legend.title = element_text(size = 7, vjust = 3),
+            legend.text =element_text(size = 6), 
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.margin = margin(t = 0,  
+                                 r = 0,  
+                                 b = 0,
+                                 l = 0)) +
+      labs(title = paste0(x$CITY[1], ": ", x$year[1]))
+  }) -> g
 
-# Map the data
+# The following gets the common legend for the maps
+# and stops it being printed 12 times -- once will be enough!
 
-breaks <- classIntervals(ipumsi$SOLIDFUEL, n = 5, style = "equal")$brks
-ggplot(data = ipumsi) +
-  annotation_map_tile(type = "cartolight", progress = "none") +
-  geom_sf(aes(fill = SOLIDFUEL), alpha = 0.8) +
-  scale_fill_gradient("%", breaks = breaks, labels = round(breaks, 1),
-                      low = "white", high = "dark red") +
-  facet_wrap(~ YEAR) +
-  xlim(c(79, 67)) +
-  ylim(c(-5, 13)) +
-  theme_minimal() +
-  labs(
-    title = "% of Children 0-5 Who Live in a Home That Cooks Using Solid Fuel",
-    subtitle = "Colombia (1985, 1993, 2005) Census Data",
-    caption = "Source: IPUMS-International"
-  )
-  
+legend <- get_legend(g[[1]])
+lapply(g, function(x) {
+  x + theme(legend.position='none')
+}) -> g
 
+# This brings all the maps together as one
 
+ggdraw(plot_grid(plot_grid(plotlist = g, ncol=3, align='v'),
+                 plot_grid(NULL, legend, ncol=1, scale = 0.5),
+                 rel_widths=c(1, 0.1),
+                 rel_heights=c(1, 0,1))) -> g
+
+# Print the maps to a .pdf file
+
+pdf("maps.pdf", paper = "a4")
+print(g)
+dev.off()
